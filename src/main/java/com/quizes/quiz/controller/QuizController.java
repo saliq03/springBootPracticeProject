2322413264d459429da4a5102d2bcceeb5f17efe
saliq03 <javidsaliq@gmail.com>
2025-08-13 18:05:00 +0530

    finished

--- src/main/java/com/quizes/quiz/controller/QuizController.java
+++ src/main/java/com/quizes/quiz/controller/QuizController.java
@@ -1,5 +1,8 @@
 package com.quizes.quiz.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,7 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.quizes.quiz.model.Response;
 import com.quizes.quiz.service.QuizService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 
@@ -20,9 +29,20 @@ public class QuizController {
    
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String title, @RequestParam int qNs,@RequestParam String category) {
-        return QuizService.createQuiz(title,qNs,category);
-        
+        return QuizService.createQuiz(title,qNs,category);     
     }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<Map<String,Object>> getQuizById(@PathVariable int id) {
+        return QuizService.getQuizById(id);
+    }
+    @PostMapping("submit/{id}")
+     public ResponseEntity<Map<String,Object>> submit(@PathVariable int id,@RequestBody List<Response> responses) {
+        return QuizService.submit(id,responses);
+    }
+    
+
+    
     
 
 }
--- src/main/java/com/quizes/quiz/service/QuizService.java
+++ src/main/java/com/quizes/quiz/service/QuizService.java
@@ -1,6 +1,10 @@
 package com.quizes.quiz.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -12,7 +16,9 @@ import org.springframework.stereotype.Service;
 import com.quizes.quiz.dao.QuestionDAO;
 import com.quizes.quiz.dao.QuizDAO;
 import com.quizes.quiz.model.Question;
+import com.quizes.quiz.model.QuestionWrapper;
 import com.quizes.quiz.model.Quiz;
+import com.quizes.quiz.model.Response;
 
 
 @Service
@@ -34,6 +40,59 @@ public class QuizService {
             return new ResponseEntity(e.toString(),HttpStatus.BAD_REQUEST); 
         }
       
+    }
+    public ResponseEntity<Map<String, Object>> getQuizById(int id) {
+        try{
+             Map<String, Object> body = new HashMap<>();
+             Optional<Quiz> quiz=quizDAO.findById(id);
+             if (quiz.isEmpty()) {
+                 return new ResponseEntity<>(Map.of("error", "Quiz not found"), HttpStatus.NOT_FOUND);
+                 }
+              body.put("title", quiz.get().getTitle());
+              body.put("id", quiz.get().getId());
+              List<Question> questions=quiz.get().getQuestions();
+              List<QuestionWrapper> questionsForUser=new ArrayList<>();
+              for (Question q : questions) {
+                QuestionWrapper userQuestion=new QuestionWrapper(q.getId(), q.getCategory(), q.getQuestion(), q.getOptionA(),q.getOptionB(), q.getOptionC(), q.getOptionD());
+                questionsForUser.add(userQuestion);
+            }
+            body.put("data",questionsForUser);
+
+         return new ResponseEntity(body,HttpStatus.OK); 
+        }
+        catch(Exception e){
+            return new ResponseEntity(e.toString(),HttpStatus.BAD_REQUEST); 
+        }
+    }
+    public ResponseEntity<Map<String, Object>> submit(int id, List<Response> responses) {
+        try{
+            Optional<Quiz> quiz=quizDAO.findById(id);
+            if (quiz.isEmpty()) {
+                 return new ResponseEntity(Map.of("error", "Quiz not found"), HttpStatus.NOT_FOUND);
+                 }
+                 
+            List<Question> questions= quiz.get().getQuestions();
+            Map<Integer,Question> questionMap=new HashMap<>();
+
+            // put quiz questions in the map acc to their id
+            for(Question q : questions){
+                questionMap.put(q.getId(), q);
+            }
+            int result=0;
+            // match answers of question with same id
+            for(Response response: responses){
+                Question question=questionMap.get(response.getId());
+                  if(response.getResponse().equals(question.getCorrectAnswer())){
+                    result++;
+                  }
+                }
+                
+            return new ResponseEntity(result,HttpStatus.OK);
+
+        }
+        catch(Exception e){
+            return new ResponseEntity(e.toString(),HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
